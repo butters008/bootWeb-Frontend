56f4ef2135ced7974d3053f274830b7b6dbd5bb6
Butters <k.butterfield08@gmail.com>
2022-04-02 21:40:26 -0500

    Completed SBA
    Completed SBA, might do minor revision

--- Frontend-SBA/js/loginLogic.js
+++ Frontend-SBA/js/loginLogic.js
@@ -6,43 +6,25 @@ const validatePassword = () =>{
     let error = document.getElementById("errorPassword");
     const regCheck1 = new RegExp("[a-zA-Z]+");
     const regCheck2 = new RegExp("[0-9]+");
-    
-    //local debug
-    console.log(password);
-    console.log(password.length);
-    
+   
     //checking the length of the password
     if(password.length >= 8 && password.length <= 20){
         console.log("inside if");
         let resultCheck1 = regCheck1.test(password);
         let resultCheck2 = regCheck2.test(password);
 
-        //local debug
-        console.log("result check 1 is " + resultCheck1);
-        console.log("result check 2 is " + resultCheck2);
-
         if(resultCheck1 == true && resultCheck2 == true){
             error.textContent = "";
-
-            //local debug
-            console.log("Passed");
-
             return true;
         }
         else{
             error.textContent = "Needs have letters and numbers in password";
             error.style.color = "red";
-
-            //local debug
-            console.log("Need letters and number");
         }    
     }
     else{
         error.textContent = "Password to short or to long - between 8 to 20 characters long";
         error.style.color = "red";
-
-        //local debug
-        console.log("Password to short");
     }
 }
 
@@ -52,24 +34,12 @@ const validateEmail = () =>{
     let error = document.getElementById("errorEmail")
     let email = document.getElementById("email").value;
     let result = re.test(email);
-    
-    //local debug
-    console.log(email);
-    console.log(result);
 
-    if (result == false)
-    {
+    if (result == false){
         error.textContent = "Please enter a valid Email"
         error.style.color = "red"
-        
-        //local debug
-        console.log("Failed");
     } else {
         error.textContent = ""
-        
-        //local debug
-        console.log("Passed");
-
         return true;
     }
 }
